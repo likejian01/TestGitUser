9a23ae1eed31cbfc5fba1303019e0cbe333d4288
likejian <13021932260@163.com>
2018-09-14 15:58:33 +0800

    xiaoli submit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,8 @@ public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
+
+        System.out.println("小李");
     }
 
 }
